cc712276710bd630052c8f6234e0e1bfb8b94c5a
ashish.g.verma <ashish.g.verma@accenture.com>
2018-03-07 19:58:50 +0530

    Updated Greetings

---
+++ heorku.md
@@ -0,0 +1,6 @@
+## Heroku Deployment Steps
+
+* heroku -v
+* heroku login (Optional)
+* heroku create
+* setup remote git repo
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({'hi': 'there'})
+    res.json({bye: 'buddy'})
 })
 
 const PORT = process.env.PORT || 5000;
