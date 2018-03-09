3c91c761189e33d6c5885fae0ebc72254afe9942
ashish.g.verma <ashish.g.verma@accenture.com>
2018-03-09 17:05:21 +0530

    Seperate dev and prd keys

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// Production keys go here.
+module.exports = {
+    'googleClientID': process.env.GOOGLE_CLIENT_ID,
+    'googleClientSecret': process.env.GOOGLE_CLIENT_SECRET,
+    'mongoURI': process.env.MONGO_URI,
+    'cookieKey': process.env.COOKIE_KEY
+}
